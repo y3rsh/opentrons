168bf7e750c9e61c4c34c0f9f3c7c3073ec540ed
amit lissack <amit@opentrons.com>
2021-04-16 12:34:37 -0400

    poller

--- api/tests/opentrons/hardware_control/test_poller.py
+++ api/tests/opentrons/hardware_control/test_poller.py
@@ -1,4 +1,3 @@
-import pytest
 from mock import AsyncMock, MagicMock
 from opentrons.hardware_control.poller import (
     Poller, Listener, Reader
@@ -6,7 +5,7 @@ from opentrons.hardware_control.poller import (
 
 
 async def test_poll_error() -> None:
-    """Test subject."""
+    """It should call error callback on error."""
     exc = AssertionError()
 
     async def raiser():
@@ -20,4 +19,17 @@ async def test_poll_error() -> None:
     await p.stop_and_wait()
 
     listener.on_error.assert_called_once_with(exc)
+    listener.on_terminated.assert_called_once()
+
+
+async def test_notify() -> None:
+    """It should call on_poll with new result."""
+    reader = AsyncMock(spec=Reader)
+    reader.read.return_value = 23
+    listener = MagicMock(spec=Listener)
+
+    p: Poller[int] = Poller(interval_seconds=.01, reader=reader, listener=listener)
+    await p.stop_and_wait()
+
+    listener.on_poll.assert_called_once_with(23)
     listener.on_terminated.assert_called_once()
