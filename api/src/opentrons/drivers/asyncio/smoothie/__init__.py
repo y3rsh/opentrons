7e53db972f31a087986124d244ab0139643e60f2
amit lissack <amit@opentrons.com>
2021-04-20 12:15:09 -0400

    start smoothie async

---
+++ api/src/opentrons/drivers/asyncio/smoothie/__init__.py
@@ -0,0 +1,12 @@
+from typing_extensions import Final
+
+
+HOMED_POSITION: Final = {
+    'X': 418.0,
+    'Y': 353.0,
+    'Z': 218.0,
+    'A': 218.0,
+    'B': 19.0,
+    'C': 19.0
+}
+Y_BOUND_OVERRIDE: Final = 370
